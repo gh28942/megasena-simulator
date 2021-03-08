37d03682fb937247711c31627778e1574ab7d8bb
gh28942 <GerH@example.com>
2021-03-08 19:20:11 +0100

    Updates

---
+++ precache-manifest.7794152469aee91488920a090a727217.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2732c38688a81a00066e613834762581",
+    "url": "/megasena-simulator/index.html"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/megasena-simulator/static/css/2.bac56067.chunk.css"
+  },
+  {
+    "revision": "8cc599e4cdc2ae0785d2",
+    "url": "/megasena-simulator/static/css/main.1091bb01.chunk.css"
+  },
+  {
+    "revision": "adc5fbcbd8be10deb8e8",
+    "url": "/megasena-simulator/static/js/2.effe3d63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/megasena-simulator/static/js/2.effe3d63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8cc599e4cdc2ae0785d2",
+    "url": "/megasena-simulator/static/js/main.3d22d367.chunk.js"
+  },
+  {
+    "revision": "48d313765e9147f27e97",
+    "url": "/megasena-simulator/static/js/runtime-main.477fd3ba.js"
+  }
+]);
